68d098a04df84b31428d27c82b4cf6d2271c0b29
kingofak83 <nanaaryan294@gmail.com>
2026-04-04 14:10:57 -0700

    sdgfsdfds

--- app.py
+++ app.py
@@ -112,6 +112,7 @@ def cleanup():
 if __name__ == '__main__':
     print("🔐 APK Encryptor Web Server")
     print("=" * 40)
-    print("Server running at: http://127.0.0.1:5000")
+    print("Server running...")
     print("Press Ctrl+C to stop")
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    
+    app.run(host='0.0.0.0', port=5000)
